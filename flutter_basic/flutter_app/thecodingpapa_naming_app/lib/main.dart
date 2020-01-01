2e8649763556a0abdd59d45f8bb5043283d845b9
nakhychoe <nakhychoe@gmail.com>
2020-01-02 03:19:54 +0900

    Add asPascalCase randomWord.

--- flutter_basic/flutter_app/thecodingpapa_naming_app/lib/main.dart
+++ flutter_basic/flutter_app/thecodingpapa_naming_app/lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final randomWord = WordPair.random();
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Naming App"),
         ),
-        body: Center(child: Text("Body Text", textScaleFactor: 1.5,)),
+        body: Center(child: Text(randomWord.asPascalCase, textScaleFactor: 1.5,)),
       ),
     );
   }
