fcc6e10eb98da797d039168814707523306e1ed9
nakhychoe <nakhychoe@gmail.com>
2020-01-01 22:47:57 +0900

    Show Text UI, Use Random Words.

--- flutter_basic/thecodingpapa_basic_clone/lib/main.dart
+++ flutter_basic/thecodingpapa_basic_clone/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -5,6 +6,8 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
     return MaterialApp(
       title: 'welcome to the coding papa',
 //      theme: ThemeData(
@@ -15,7 +18,7 @@ class MyApp extends StatelessWidget {
           title: Text("Best app"),
         ),
         body: Center(
-          child: Text("Hello there"),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
